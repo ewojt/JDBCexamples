f50ebfcbb4ba40e5b8fb28770a7ed8b7e8accadc
Iwona W <iwoniwa@gmail.com>
2015-01-04 17:15:49 +0100

    First working version

--- src/main/java/eu/ewojt/jdbc/JDBCexamples.java
+++ src/main/java/eu/ewojt/jdbc/JDBCexamples.java
@@ -5,9 +5,23 @@ package eu.ewojt.jdbc;
 public class JDBCexamples {
 
 	public static void main(String[] argv) {
-       //Table USER is created
-	   User user = new User();
        
+	   // Table USER is created
+	   User user = new User();
+	   // Inserts  
+	   user.insertUserStatement();
+	   user.insertUserPreStatement(99, "zuza", "Zuzanna", "Iksinska", "admin1");
+	   user.insertUserPreStatement(999, "kotek", "Tomek", "Igrek", "admin1");
+	   // Update
+	   user.updateUserStatement();
+	   user.updateUserPreStatement("zuzka2", 99);
+	   // Delete
+	   user.deleteUserStatement();
+	   user.deleteUserPreStatement(99);
+	   // Batch
+	   user.batchStatement();
+	   user.batchPreStatement();
+	   
        user.closeConnection();
 	}
 }
--- src/main/java/eu/ewojt/jdbc/User.java
+++ src/main/java/eu/ewojt/jdbc/User.java
@@ -6,8 +6,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -16,8 +14,6 @@ public class User {
 	private static final String CONNECTION = "jdbc:mysql://localhost/test";
 	private static final String USER = "root";
 	private static final String PASSWORD = "password";
-	private static final DateFormat dateFormat = new SimpleDateFormat(
-			"yyyy/MM/dd HH:mm:ss");
 
 	private Connection conn;
 	private Statement stat;
@@ -45,14 +41,13 @@ public class User {
 		String createTable = "CREATE TABLE USER(" + "USER_ID INT(5) NOT NULL, "
 				+ "USERNAME VARCHAR(20) NOT NULL, "
 				+ "FIRSTNAME VARCHAR(10) NOT NULL, "
-				+ "LASTSTNAME VARCHAR(10) NOT NULL, "
+				+ "LASTNAME VARCHAR(10) NOT NULL, "
 				+ "CREATED_BY VARCHAR(20) NOT NULL, "
-				+ "CREATED_DATE DATE NOT NULL, " + "PRIMARY KEY (USER_ID) "
+				+ "PRIMARY KEY (USER_ID) "
 				+ ")";
 		try {
 			stat.execute(createTable);
 			System.out.println("Table \"USER\" is created!");
-			System.out.println(createTable);
 		} catch (SQLException e) {
 			System.err.println("Blad przy tworzeniu tabeli \"USER\"");
 			e.printStackTrace();
@@ -64,16 +59,13 @@ public class User {
 	// Insert a record into table via JDBC Statement
 	public boolean insertUserStatement() {
 		String insertUser = "INSERT INTO USER"
-				+ "(USER_ID, USERNAME, FIRSTNAME, LASTNAME, CREATED_BY, CREATED_DATE) "
-				+ "VALUES" + "(1,'misiek','Jan','Kowalski','system', "
-				+ "to_date('" + getCurrTimeStamp()
-				+ "', 'yyyy/mm/dd hh24:mi:ss'))";
+				+ "(USER_ID, USERNAME, FIRSTNAME, LASTNAME, CREATED_BY) "
+				+ "VALUES" + "(1,'misiek','Jan','Kowalski','system')";
 		try {
 			stat.execute(insertUser);
-			System.out.println("U¿ytkownik dodany");
-			System.out.println(insertUser);
+			System.out.println("U¿ytkownik dodany MISIEK");
 		} catch (SQLException e) {
-			System.err.println("Blad przy dodawaniu u¿ytkownika");
+			System.err.println("Blad przy dodawaniu u¿ytkownika - misiek");
 			e.printStackTrace();
 			return false;
 		}
@@ -84,22 +76,20 @@ public class User {
 	public boolean insertUserPreStatement(int id, String login, String name,
 			String sname, String who) {
 		String insertUser = "INSERT INTO USER"
-				+ "(USER_ID, USERNAME, FIRSTNAME, LASTNAME, CREATED_BY, CREATED_DATE) "
-				+ "VALUES" + "(?,?,?,?,?,?)";
+				+ "(USER_ID, USERNAME, FIRSTNAME, LASTNAME, CREATED_BY) "
+				+ "VALUES" + "(?,?,?,?,?)";
 		try {
-			PreparedStatement preparedStatement = conn
-					.prepareStatement(insertUser);
+			PreparedStatement preparedStatement = conn.prepareStatement(insertUser);
 			preparedStatement.setInt(1, id);
 			preparedStatement.setString(2, login);
 			preparedStatement.setString(3, name);
 			preparedStatement.setString(4, sname);
 			preparedStatement.setString(5, who);
-			preparedStatement.setTimestamp(6, getCurrentTimeStamp());
 			preparedStatement.executeUpdate();
 
-			System.out.println("Record dodany");
+			System.out.println("U¿ytkownik dodany - " + login);
 		} catch (SQLException e) {
-			System.err.println("Blad przy dodawaniu u¿ytkownika");
+			System.err.println("Blad przy dodawaniu u¿ytkownika - " + login);
 			e.printStackTrace();
 			return false;
 		}
@@ -112,10 +102,9 @@ public class User {
 				+ " WHERE USER_ID = 1";
 		try {
 			stat.execute(updateUser);
-			System.out.println("U¿ytkownik zmodifikowany");
-			System.out.println(updateUser);
+			System.out.println("U¿ytkownik zmodifikowany - monky");
 		} catch (SQLException e) {
-			System.err.println("Blad przy modifikowaniu u¿ytkownika");
+			System.err.println("Blad przy modifikowaniu u¿ytkownika = monky");
 			e.printStackTrace();
 			return false;
 		}
@@ -132,10 +121,9 @@ public class User {
 			preparedStatement.setString(1, login);
 			preparedStatement.setInt(2, id);
 			preparedStatement.executeUpdate();
-			System.out.println("U¿ytkownik zmodifikowany");
-			System.out.println(updateUser);
+			System.out.println("U¿ytkownik zmodifikowany - " + login);
 		} catch (SQLException e) {
-			System.err.println("Blad przy modifikowaniu u¿ytkownika");
+			System.err.println("Blad przy modifikowaniu u¿ytkownika - " + login);
 			e.printStackTrace();
 			return false;
 		}
@@ -144,70 +132,74 @@ public class User {
 
 	// Delete a record into table via JDBC Statement
 	public boolean deleteUserStatement() {
-		String deleteUser = "DELETE USER WHERE USER_ID = 1";
+		String deleteUser = "DELETE FROM USER WHERE USER_ID = 999";
 		try {
 			stat.execute(deleteUser);
-			System.out.println("U¿ytkownik usuniêty");
-			System.out.println(deleteUser);
+			System.out.println("U¿ytkownik usuniêty id: 999");
 		} catch (SQLException e) {
-			System.err.println("Blad przy usuwaniu u¿ytkownika");
+			System.err.println("Blad przy usuwaniu u¿ytkownika id: 999");
 			e.printStackTrace();
 			return false;
 		}
 		return true;
 	}
-	
+
 	// Delete a record into table via JDBC PreparedStatement
 	public boolean deleteUserPreStatement(int id) {
-		String deleteUser = "DELETE USER WHERE USER_ID = ?";
+		String deleteUser = "DELETE FROM USER WHERE USER_ID = ?";
 		try {
 			PreparedStatement preparedStatement = conn
 					.prepareStatement(deleteUser);
 			preparedStatement.setInt(1, id);
 			preparedStatement.executeUpdate();
-			System.out.println("U¿ytkownik usuniêty");
-			System.out.println(deleteUser);
+			System.out.println("U¿ytkownik usuniêty id:" + id);
 		} catch (SQLException e) {
-			System.err.println("Blad przy usuwaniu u¿ytkownika");
+			System.err.println("Blad przy usuwaniu u¿ytkownika id:" + id);
 			e.printStackTrace();
 			return false;
 		}
 		return true;
 	}
-	
-	//Select records from table via JDBC Statement
-    public List<String> selectUserStatement() {
-        List<String> users = new LinkedList<String>();
-        String selectUser = "SELECT USER_ID, USERNAME from USER";
-        try {
-            ResultSet result = stat.executeQuery(selectUser);
-            while(result.next()) {
-               String newstr = "ID uzytkownika: " + result.getString("USER_ID") + " Login: " + result.getString("USERNAME");   
-               users.add(new String(newstr));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
-        return users;
-    }
-    
-	//Select records from table via JDBC Statement
-    public String selectUserPreStatement() {
-    	String newstr = null;
-        String selectUser = "SELECT USER_ID, USERNAME from USER WHERE USER_ID = ?";
-        try {
-            ResultSet result = stat.executeQuery(selectUser);
-            while(result.next()) {
-               newstr = "ID uzytkownika: " + result.getString("USER_ID") + " Login: " + result.getString("USERNAME");   
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
-        return newstr;
-    }
-		
+
+	// Select records from table via JDBC Statement
+	public List<String> selectUserStatement() {
+		List<String> users = new LinkedList<String>();
+		String selectUser = "SELECT USER_ID, USERNAME from USER";
+		try {
+			ResultSet result = stat.executeQuery(selectUser);
+			while (result.next()) {
+				String newstr = "ID uzytkownika: "
+						+ result.getString("USER_ID") + " Login: "
+						+ result.getString("USERNAME");
+				users.add(new String(newstr));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return users;
+	}
+
+	// Select records from table via JDBC Statement
+	public String selectUserPreStatement(int id) {
+		String newstr = null;
+		String selectUser = "SELECT USER_ID, USERNAME from USER WHERE USER_ID = ?";
+		try {
+			PreparedStatement preparedStatement = conn
+					.prepareStatement(selectUser);
+			preparedStatement.setInt(1, id);
+			ResultSet result = preparedStatement.executeQuery(selectUser);
+			while (result.next()) {
+				newstr = "ID uzytkownika: " + result.getString("USER_ID")
+						+ " Login: " + result.getString("USERNAME");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return newstr;
+	}
+
 	public void closeConnection() {
 		try {
 			conn.close();
@@ -217,14 +209,71 @@ public class User {
 		}
 	}
 
-	private String getCurrTimeStamp() {
+	// Batch process via JDBC Statement
+	public boolean batchStatement() {
+		String insertUser1 = "INSERT INTO USER"
+				+ "(USER_ID, USERNAME, FIRSTNAME, LASTNAME, CREATED_BY) "
+				+ "VALUES" + "(101,'us1','Zosia','Kapusta','root' )";
 
-		java.util.Date today = new java.util.Date();
-		return dateFormat.format(today.getTime());
+		String insertUser2 = "INSERT INTO USER"
+				+ "(USER_ID, USERNAME, FIRSTNAME, LASTNAME, CREATED_BY) "
+				+ "VALUES" + "(102,'us2','Marek','Kalafior','root' )";
+
+		String insertUser3 = "INSERT INTO USER"
+				+ "(USER_ID, USERNAME, FIRSTNAME, LASTNAME, CREATED_BY) "
+				+ "VALUES" + "(103,'us3','Tomy','Lee','root')";
+
+		try {
+			conn.setAutoCommit(false);
+			stat.addBatch(insertUser1);
+			stat.addBatch(insertUser2);
+			stat.addBatch(insertUser3);
+			stat.executeBatch();
+			conn.commit();
+			System.out.println("U¿ytkownicy dodani!");
+		} catch (SQLException e) {
+			System.err.println("Blad przy dodawaniu u¿ytkowników");
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
+	
+	// Batch process via JDBC PreparedStatement
+	public boolean batchPreStatement() {
+		String insertUser = "INSERT INTO USER"
+			+ "(USER_ID, USERNAME, FIRSTNAME, LASTNAME, CREATED_BY) "
+			+ "VALUES" + "(?,?,?,?,?)";
 
-	private static java.sql.Timestamp getCurrentTimeStamp() {
-		java.util.Date today = new java.util.Date();
-		return new java.sql.Timestamp(today.getTime());
+		try {
+			//Commit transaction manually
+			conn.setAutoCommit(false);
+			PreparedStatement preparedStatement = conn.prepareStatement(insertUser);
+			 
+			preparedStatement.setInt(1, 10);
+			preparedStatement.setString(2, "us10");
+			preparedStatement.setString(3, "Liliy");
+			preparedStatement.setString(4, "Cotty");
+			preparedStatement.setString(5, "root");
+			
+			preparedStatement.addBatch();
+			 
+			preparedStatement.setInt(1, 11);
+			preparedStatement.setString(2, "us11");
+			preparedStatement.setString(3, "Mike");
+			preparedStatement.setString(4, "Knigth");
+			preparedStatement.setString(5, "root");
+			
+			preparedStatement.addBatch();
+			
+			preparedStatement.executeBatch();
+			conn.commit();
+			System.out.println("U¿ytkownicy dodani!");
+		} catch (SQLException e) {
+			System.err.println("Blad przy dodawaniu u¿ytkowników");
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 }
